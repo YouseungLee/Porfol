994cab2d517290bbfcf195269ee7ab7f0c24f971
youseunglee <dldbtmd0502@gmail.com>
2022-08-12 18:40:21 +0900

    2022-08-12

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,16 @@
-let header = document.querySelector(".header");
+const line = document.querySelector('.line');
+const titleContent = document.querySelector('.title-container');
+let shown = true;
+
+blinkLine = () => {
+    if (shown == true) {
+        line.style.display = "none";
+        shown = false;
+    } else {
+        line.style.display = "block";
+        shown = true;
+    }
+}
+
+setInterval(blinkLine, 500);
+
